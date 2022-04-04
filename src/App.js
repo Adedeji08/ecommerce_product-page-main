eb1a9d44510fb370b40fa73d9bf5510f934b23a7
Damilola Adedeji <damilola.adedeji@Terra-098.local>
2022-04-04 14:01:22 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Cart from "./components/Cart";
+import Lightbox from "./components/Lightbox";
+import Navbar from "./components/Navbar";
+import ProductDescription from "./components/ProductDescription";
+import ProductImages from "./components/ProductImages.js";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Navbar />
+      <Lightbox />
+      <div className="hero">
+        <ProductImages />
+        <ProductDescription/>
+      </div>
+      <Cart />
+      <Sidebar />
     </div>
   );
 }
